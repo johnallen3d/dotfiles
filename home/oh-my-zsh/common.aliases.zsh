216d6653bd18eb980f9c274ab2481b5d8840ae9b
John Allen <john@threedogconsulting.com>
2012-12-23 19:20:27 -0500

    adds oh-my-zsh alias files

---
+++ home/oh-my-zsh/common.aliases.zsh
@@ -0,0 +1,16 @@
+# rails
+alias rfs='RACK_ENV=development foreman start'
+alias rake='bundle exec rake'
+alias beg='bundle exec guard'
+
+# zeus
+alias zs='zeus start'
+alias zc='zeus console'
+alias zt='zeus test'
+alias zr='zeus rake'
+
+# testing
+alias rss='bundle exec rspec spec'
+
+# gems
+alias gem install='gem install --no-rdoc --no-ri'
---
+++ home/oh-my-zsh/local.aliases.zsh
@@ -0,0 +1,30 @@
+# heroku
+alias hlogqa='heroku logs --tail --remote qa'
+alias hlogp='heroku logs --tail --remote prod'
+alias hconqa='heroku run console --remote qa'
+alias hconp='heroku run console --remote prod'
+alias hpsqa='heroku ps --remote qa'
+alias hpsp='heroku ps --remote prod'
+
+# qa & prod ssh
+alias ssqa='ssh deploy@tk-qa'
+alias ssp='ssh deploy@tk-prod'
+
+# devbox
+alias vup="cd $DEV_BOX;vagrant up;"
+alias vhalt="cd $DEV_BOX; vagrant halt;"
+alias vssh="cd $DEV_BOX; vagrant ssh;"
+alias apps="cd $DEV_BOX/apps"
+alias gems="cd $DEV_BOX/gems"
+
+function run_command_on_devbox() {
+  ssh -p 2222 -i ~/.vagrant.d/insecure_private_key vagrant@localhost "source ~/.bash_profile; $1"
+}
+
+function restart_app() {
+  run_command_on_devbox "restart_app $1"
+}
+
+function bundle_app() {
+  run_command_on_devbox "cd /var/apps/$1_app; bundle; restart_app $1"
+}
