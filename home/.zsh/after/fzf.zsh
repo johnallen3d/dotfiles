e2fad10f1e2c223dc07b711fbb846af0a8f47e4f
John Allen <john@threedogconsulting.com>
2017-03-16 08:05:44 -0400

    Modernize fzf config for zsh

--- home/.zsh/after/fzf.zsh
+++ home/.zsh/after/fzf.zsh
@@ -1,30 +1,27 @@
-HEAD="0.15.1"
 # Setup fzf
 # ---------
-if [[ ! "$PATH" =~ "/usr/local/Cellar/fzf/$HEAD/bin" ]]; then
-  export PATH="$PATH:/usr/local/Cellar/fzf/$HEAD/bin"
+if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+  export PATH="$PATH:/usr/local/opt/fzf/bin"
 fi
 
 # Man path
 # --------
-if [[ ! "$MANPATH" =~ "/usr/local/Cellar/fzf/$HEAD/man" && -d "/usr/local/Cellar/fzf/$HEAD/man" ]]; then
-  export MANPATH="$MANPATH:/usr/local/Cellar/fzf/$HEAD/man"
+if [[ ! "$MANPATH" == */usr/local/opt/fzf/man* && -d "/usr/local/opt/fzf/man" ]]; then
+  export MANPATH="$MANPATH:/usr/local/opt/fzf/man"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- =~ i ]] && source "/usr/local/Cellar/fzf/$HEAD/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/Cellar/fzf/$HEAD/shell/key-bindings.zsh"
+source "/usr/local/opt/fzf/shell/key-bindings.zsh"
 
-# Use ag instead of the default find command for listing candidates.
-# - The first argument to the function is the base path to start traversal
-# - Note that ag only lists files not directories
-# - See the source code (completion.{bash,zsh}) for the details.
+# Search tool
 # ------------
-_fzf_compgen_path() {
-  ag -g "" "$1"
-}
-export FZF_DEFAULT_COMMAND='ag -g ""'
+export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
+
+# Load fzf
+# ------------
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
