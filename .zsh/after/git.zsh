e1e44e44469fb3d3c53c79740f8f7d0e2b7a8d76
John Allen <john@threedogconsulting.com>
2017-07-12 19:01:37 -0400

    Add additional git related alias shortcuts

--- .zsh/after/git.zsh
+++ .zsh/after/git.zsh
@@ -7,6 +7,7 @@ alias gpf="git push --force-with-lease"
 alias gl="git pull"
 alias gco="git checkout"
 alias gm='git merge --ff-only'
+alias gcf='git commit --fixup'
 
 # too many times have i typed gs and stashed when i meant to type gst!!!
 unalias gs &> /dev/null
@@ -17,6 +18,7 @@ function delete-branch() {
   git push origin :$branch
   git branch -D $branch
 }
+alias db=delete-branch
 
 # hub
 fpath=(~/.zsh/completions $fpath)
