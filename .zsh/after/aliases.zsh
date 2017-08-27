c21181b94285dc045723ead9ba379f6641d24276
John Allen <john@threedogconsulting.com>
2017-08-26 22:12:06 -0400

    Fix typo in comment

--- .zsh/after/aliases.zsh
+++ .zsh/after/aliases.zsh
@@ -2,6 +2,6 @@ alias nekes='cd ~/development/technekes'
 alias dfm-ssh='screen $(readlink $HOME/Library/Containers/com.docker.docker/Data/com.docker.driver.amd64-linux/tty)'
 alias irb='docker-ruby'
 
-# home flder alias for git, see here for more details
+# home folder alias for git, see here for more details
 # https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotfiles-git-bare-repo/
 alias config='/usr/bin/git --git-dir=/Users/johna/.cfg/ --work-tree=/Users/johna'
