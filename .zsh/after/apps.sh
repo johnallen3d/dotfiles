318a1ef912d002d6acaffea7cd5532a87e0dca82
John Allen <john@threedogconsulting.com>
2017-10-23 15:43:26 -0400

    Prefer whalebrew over custom functions

--- .zsh/after/apps.sh
+++ .zsh/after/apps.sh
@@ -1,20 +1,6 @@
 #
 # msic apps as docker containers
 #
-function aws() {
-  docker run \
-    -it \
-    --rm \
-    --volume $HOME/.aws:/root/.aws \
-    --volume $PWD:/tmp \
-    --workdir /tmp \
-    technekes/awscli "$@"
-}
-
-function neovim() {
-  # opens in neovim-dot-app
-  open -a Neovim $*
-}
 
 function codeclimate() {
   docker run \
@@ -28,47 +14,11 @@ function codeclimate() {
     codeclimate/codeclimate "${1:-help}"
 }
 
-function jq() {
-  docker run \
-    -i \
-    -v $(pwd):$(pwd) \
-    -w $(pwd) \
-    --rm \
-    colstrom/jq "$@"
-}
-
-# function heroku() {
-#   docker run \
-#     --rm \
-#     -it \
-#     -v $HOME/.netrc:/root/.netrc \
-#     # -v $HOME/.heroku:/root/.heroku \
-#     -v $PWD:/usr/src/app \
-#     -w /usr/src/app \
-#     technekes/heroku-toolbelt "$@"
-# }
-
-function http() {
-  docker run \
-    -t \
-    --rm \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    --log-driver none \
-    jess/httpie "$@"
-}
-
 function psql() {
   docker run \
     --rm \
     -it \
     governmentpaas/psql "$@"
-}
-
-function speedtest() {
-  docker run \
-    --rm \
-    --net=host \
-    tianon/speedtest
 }
 
 function yo() {
