ae3f12c057c75d340e99f65235799406c5f30267
John Allen <john@threedogconsulting.com>
2022-11-10 13:53:19 -0500

    fix(nvim): remove `packer` bootstrap
    Just pre-install globally via `chezmoi`.

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,24 +1,3 @@
-local ensure_packer = function()
-	local fn = vim.fn
-	local install_path = fn.stdpath("data")
-		.. "/site/pack/packer/start/packer.nvim"
-	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({
-			"git",
-			"clone",
-			"--depth",
-			"1",
-			"https://github.com/wbthomason/packer.nvim",
-			install_path,
-		})
-		vim.cmd([[packadd packer.nvim]])
-		return true
-	end
-	return false
-end
-
-local packer_bootstrap = ensure_packer()
-
 return require("packer").startup({
 	function(use)
 		-- Packer can manage itself as an optional plugin
@@ -316,12 +295,6 @@ return require("packer").startup({
 			as = "colorizer",
 			opt = true,
 		})
-
-		-- Automatically set up your configuration after cloning packer.nvim
-		-- Put this at the end after all plugins
-		if packer_bootstrap then
-			require("packer").sync()
-		end
 	end,
 
 	profile = {
