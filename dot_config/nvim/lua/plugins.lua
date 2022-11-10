387fbadf0216459959412f652f622e4155572277
John Allen <john@threedogconsulting.com>
2022-11-10 11:13:43 -0500

    feat(nvim): allow `packer.nvim` to boostrap itself

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,20 +1,26 @@
--- local install_path = vim.fn.stdpath("data")
--- 	.. "/site/pack/packer/opt/packer.nvim"
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath("data")
+		.. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({
+			"git",
+			"clone",
+			"--depth",
+			"1",
+			"https://github.com/wbthomason/packer.nvim",
+			install_path,
+		})
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
+end
 
--- if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
--- 	vim.api.nvim_command.execute(
--- 		"!git clone https://github.com/wbthomason/packer.nvim " .. install_path
--- 	)
--- end
+local packer_bootstrap = ensure_packer()
 
--- Only required if you have packer in your `opt` pack
-vim.cmd.packadd("packer.nvim")
-
-local packer = require("packer")
-local use = packer.use
-
-packer.startup({
-	function()
+return require("packer").startup({
+	function(use)
 		-- Packer can manage itself as an optional plugin
 		use({
 			"wbthomason/packer.nvim",
@@ -310,6 +316,12 @@ packer.startup({
 			as = "colorizer",
 			opt = true,
 		})
+
+		-- Automatically set up your configuration after cloning packer.nvim
+		-- Put this at the end after all plugins
+		if packer_bootstrap then
+			require("packer").sync()
+		end
 	end,
 
 	profile = {
