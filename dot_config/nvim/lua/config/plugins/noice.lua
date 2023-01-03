358d6229c9825481a4c40818b015e0c5c162cf52
John Allen <john@threedogconsulting.com>
2023-01-03 12:25:33 -0500

    feat(nvim): enable inc-rename/noice integration

--- dot_config/nvim/lua/config/plugins/noice.lua
+++ dot_config/nvim/lua/config/plugins/noice.lua
@@ -9,6 +9,7 @@ return {
 		presets = {
 			long_message_to_split = true, -- long messages will be sent to a split
 			lsp_doc_border = true, -- add a border to hover docs and signature help
+			inc_rename = true, -- enables an input dialog for inc-rename.nvim
 		},
 		lsp = {
 			override = {
