ea657fc8118166a07b935b5cc2280ed338bb24eb
John Allen <john@threedogconsulting.com>
2023-01-03 12:25:05 -0500

    feat(nvim): add return to last position auto cmd

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -78,12 +78,15 @@ vim.api.nvim_create_autocmd("FileChangedShellPost", {
 	group = "AutoReload",
 })
 
-vim.api.nvim_create_autocmd("BufWritePost", {
-	pattern = "lua/plugins.lua",
+vim.api.nvim_create_augroup("ReturnToLastPosition", { clear = true })
+
+vim.api.nvim_create_autocmd("BufReadPost", {
 	callback = function()
-		util.packer.compile()
-		vim.notify("Packer has been re-compiled", "info", {
-			title = "PackerCompile executed",
-		})
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
 	end,
+	group = "ReturnToLastPosition",
 })
