aa7845fcc0820688db43f9ee5219b4763eda404c
John Allen <john@threedogconsulting.com>
2023-02-26 18:17:44 -0500

    feat(nvim): add mason-tool-installer for better ux

--- dot_config/nvim/lua/config/plugins/mason.lua
+++ dot_config/nvim/lua/config/plugins/mason.lua
@@ -2,6 +2,7 @@ return {
 	"williamboman/mason.nvim",
 	dependencies = {
 		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
 	},
 	config = function()
 		require("mason").setup()
@@ -20,30 +21,56 @@ return {
 			"lua_ls",
 			"tsserver",
 			"yamlls",
-			-- -- DAP
-			-- "debugpy",
-			-- -- Formatters
-			-- "black",
-			-- "cbfmt",
-			-- "golines",
-			-- "isort",
-			-- "prettier",
-			-- "standardrb",
-			-- "shfmt",
-			-- "stylua",
-			-- -- Linters
-			-- "codespell",
-			-- "eslint_d",
-			-- "flake8",
-			-- "markdownlint",
-			-- "shellcheck",
-			-- "sqlfluff",
-			-- "write-good",
 		}
 
 		require("mason-lspconfig").setup({
 			ensure_installed = servers,
-			automatic_installation = true,
+			automatic_installation = false,
+		})
+
+		require("mason-tool-installer").setup({
+			ensure_installed = {
+				"bash-language-server",
+				"black",
+				"cbfmt",
+				"codespell",
+				"commitlint",
+				"cueimports",
+				"cuelsp",
+				"debugpy",
+				"docker-compose-language-service",
+				"dockerfile-language-server",
+				"erg-language-server",
+				"eslint-lsp",
+				"flake8",
+				"gofumpt",
+				"goimports",
+				"golangci-lint",
+				"golines",
+				"gopls",
+				"hadolint",
+				"html-lsp",
+				"isort",
+				"json-lsp",
+				"json-to-struct",
+				"lua-language-server",
+				"markdownlint",
+				"prettier",
+				"pyright",
+				"revive",
+				"rust-analyzer",
+				"rustfmt",
+				"shellcheck",
+				"shfmt",
+				"solargraph",
+				"sqlfluff",
+				"stylua",
+				"typescript-language-server",
+				"yaml-language-server",
+			},
+
+			auto_update = false,
+			run_on_start = false,
 		})
 	end,
 }
