04b01b5d13dbc8ee6960ff30e134bc150d6709d8
John Allen <john@threedogconsulting.com>
2023-03-06 19:57:15 -0500

    feat(nvim): set LocalLeader

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -14,6 +14,7 @@ vim.opt.runtimepath:prepend(lazypath)
 
 -- make sure to set `mapleader` before lazy so your mappings are correct
 vim.g.mapleader = ","
+vim.g.maplocalleader = ","
 
 -- install/load plugins
 require("lazy").setup("config.plugins")
