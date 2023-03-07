c23dbae3588fd8c328f6365136e9af0f29184a61
John Allen <john@threedogconsulting.com>
2023-03-06 19:56:18 -0500

    feat(nvim): change neorg icons

--- dot_config/nvim/lua/config/plugins/neorg.lua
+++ dot_config/nvim/lua/config/plugins/neorg.lua
@@ -6,7 +6,11 @@ return {
 		opts = {
 			load = {
 				["core.defaults"] = {}, -- Loads default behaviour
-				["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
+				["core.norg.concealer"] = { -- Adds pretty icons to your documents
+					config = {
+						icon_preset = "diamond",
+					},
+				},
 				["core.norg.dirman"] = { -- Manages Neorg workspaces
 					config = {
 						workspaces = {
