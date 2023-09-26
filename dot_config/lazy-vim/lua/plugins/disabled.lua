e9cadfd1e6368a7f90278e7e7df849647afbd94e
John Allen <john@threedogconsulting.com>
2023-09-26 18:35:55 -0400

    chore(nvim): disable "pairs" plugin

--- dot_config/lazy-vim/lua/plugins/disabled.lua
+++ dot_config/lazy-vim/lua/plugins/disabled.lua
@@ -1,7 +1,7 @@
 return {
-	-- disable trouble
-	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
 	{ "akinsho/bufferline.nvim", enabled = false },
-	{ "goolord/alpha-nvim", enabled = false },
+	{ "echasnovski/mini.pairs", enabled = false },
 	{ "folke/persistence.nvim", enabled = false },
+	{ "goolord/alpha-nvim", enabled = false },
+	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
